98692635e681ba89dc79db63a7e427fa9bb37ebf
Dylan Sleith <sleithdylan@gmail.com>
2020-10-20 14:16:12 +0100

    Add movie component

---
+++ src/components/Create.js
@@ -0,0 +1,14 @@
+// Imports React
+import React from 'react';
+
+// Exports Create Component
+export class Create extends React.Component {
+  render() {
+    return (
+      // <div> has been replaced with a React Fragment
+      <>
+        <h1>This is the create component.</h1>
+      </>
+    );
+  }
+}
